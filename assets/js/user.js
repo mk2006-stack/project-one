c4c3c1d93fd034c16a8083d2cd57bb07f3da4957
berlin <mohammad>
2026-02-03 16:32:58 +0330

    part11

--- assets/js/user.js
+++ assets/js/user.js
@@ -14,5 +14,37 @@ document.addEventListener('DOMContentLoaded', function() {
         e.target.classList.add('dis-none')
     })
 
+setTimeout(function(){
+  console.log("start...");
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((json) => {
+    console.log("end...");
+     console.log(json)
 
-  });           
+       let html = "" 
+
+     for (const user of json){
+
+    html = html + `<tr>
+                   <td>${user.id}</td>
+                   <td> ${user.name} </td>
+                   <td>${user.username}</td>
+                   <td>${user.phone}</td>
+                   <td>${user.email}</td>
+                   <td>${user.address.city}</td>
+                   <td><i class="material-icons red-text darken-3  m-r-5 m-r-5 pos-relative top-7">delete_forever</i>
+                   <i class="material-icons green-text darken-3  m-l-5 m-r-5 pos-relative top-7 " >edit</i></td>
+               </tr>` 
+
+     }
+
+      document.querySelector('.desktop_table_body').innerHTML = html
+
+
+    
+
+});
+},5000)
+  
+  });     
