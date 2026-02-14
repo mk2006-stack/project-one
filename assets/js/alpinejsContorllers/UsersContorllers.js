e47c4635b8940264ddf523337aa28a95721c16f9
berlin <mohammad>
2026-02-14 15:22:56 +0330

    part17

--- assets/js/alpinejsContorllers/UsersContorllers.js
+++ assets/js/alpinejsContorllers/UsersContorllers.js
@@ -2,18 +2,27 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('usersData', ()=>({
          users:[],
+         pageUsers: [],
          isLoading: false,
-         showAddModal : false,   
+         showAddModal : false,
+         pageCount: 1,
+         itemsCount: 4,
+         currentPage: 1,
          getUsers(){
              this.isLoading = true
              axios.get("https://jsonplaceholder.typicode.com/users").then((res)=>{
                this.users= res.data
-               this.isLoading = false
-             }).catch(error =>{
-                 console.log(error.massage);
+               this.pagination()
              }).finally(()=>{
                  this.isLoading = false 
              }) 
+         },
+         pagination(){
+            this.pageCount = Math.ceil(this.users.length / this.itemsCount)
+            let start = (this.currentPage * this.itemsCount ) - this.itemsCount
+            let end = this.currentPage * this.itemsCount
+            this.pageUsers = this.users.slice(start,end)
+            console.log(this.pageUsers);
          }
      }))
 
