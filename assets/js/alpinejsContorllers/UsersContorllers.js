ca1e9d0ee4cf84e202c56164820a5f4890b82c4c
berlin <mohammad>
2026-07-28 17:46:18 +0330

    part21

--- assets/js/alpinejsContorllers/UsersContorllers.js
+++ assets/js/alpinejsContorllers/UsersContorllers.js
@@ -56,6 +56,16 @@ document.addEventListener('alpine:init', () => {
         },
         handleSubmitaddUserForm(){
              console.log(this.newUserInfo);
+              axios.post("https://jsonplaceholder.typicode.com/users" , this.newUserInfo , ).then
+              ((res)=>{
+                if (res.status == 201 )
+               this.mainUsers.push(res.data)
+               this.showAddModal= false
+               this.pagination() 
+               
+             }).finally(()=>{
+                 this.isLoading = false 
+             }) 
         }
      }))
 
